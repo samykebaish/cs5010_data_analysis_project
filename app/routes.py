b2cbfd3ffec2f63afab5696c58110b15e2be4b28
Samy Kebaish <iterativeimprovement@gmail.com>
2021-04-18 11:01:17 -0400

    Beginnings of Front End

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,13 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return "Hellp, world!"
+    data = {'Data': 'Dataset'}
+    return render_template('index.html', title='Home', data=data)
+
+
+@app.route('/data_dictionary')
+def data_dictionary():
+    return render_template('about.html', title='Data Dictionary', data=data)
